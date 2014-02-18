9ee14cb145cc5fe46682f2ef5fe2d5860b063d3a
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2014-02-18 21:48:22 +0200

    jsonp_callback moved to top level function

--- dating/js/app.js
+++ dating/js/app.js
@@ -20,10 +20,11 @@ datingSite.controller('LanguageController', function($scope, $translate){
 
 
 
+function jsonp_callback(data){
+    console.log(data);
+}
+
 datingSite.controller('PhotoController', ['$scope', '$http', '$translate', function ($scope, $http, $translate) {
-    var jsonp_callback = function(data){
-        console.log(data);
-    }
 
     $http.jsonp('http://217.196.165.81:8983/solr/dating/query?q=sex:woman&wt=json&json.wrf=jsonp_callback')
         .success(function(data, status, headers, config){
