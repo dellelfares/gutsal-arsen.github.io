27732980d99399d6273df1549f4d9ba19d2d21c7
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2014-02-19 13:11:09 +0200

    fixed profile collection

--- dating/js/app.js
+++ dating/js/app.js
@@ -24,9 +24,9 @@ datingSite.controller('PhotoController', ['$scope', '$http', '$translate', funct
     $http.jsonp('http://217.196.165.81:8983/solr/dating/query?q=sex:woman&wt=json&json.wrf=JSON_CALLBACK')
         .success(function(data, status, headers, config){
             $scope.profiles = [];
-            var profile = {};
             console.log('Success', data)
             for(var idx in data.response.docs){
+                var profile = {};
                 var doc = data.response.docs[idx]
                 for(field in doc){
                     profile[field] = doc[field];
